d728d3341c9ca13fd349d219a43f1fba17bbe861
sittler-ines <ines.sittler@edu.univ-fcomte.fr>
2025-11-13 15:23:13 +0100

    ouverture menu

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+const toggle = document.querySelector(".menu-btn");
+const nav = document.querySelector("nav.menu");
+
+toggle.addEventListener("click", () => {
+  // Mise à jour des attributs ARIA pour accessibilité
+  nav.setAttribute("aria-hidden","false");
+  toggle.setAttribute("aria-expanded","true");
+});
