eff3caa49a7046a03f2709ec7c632bf7481e5c35
sittler-ines <ines.sittler@edu.univ-fcomte.fr>
2025-11-13 15:30:52 +0100

    noscroll

--- src/js/script.js
+++ src/js/script.js
@@ -4,5 +4,6 @@ const nav = document.querySelector("nav.menu");
 toggle.addEventListener("click", () => {
   const isOpen = toggle.getAttribute("aria-expanded") === "true";
   nav.setAttribute("aria-hidden", String(isOpen)); 
-  toggle.setAttribute("aria-expanded", String(!isOpen)); 
+  toggle.setAttribute("aria-expanded", String(!isOpen));
+  document.body.classList.toggle('noscroll'); 
 });
